2b02d5e6a6c2a2e89e39bdf53cb997a55486f21c
zhouzihong <1635757679@qq.com>
2024-04-17 16:20:56 +0800

    删除不必要文件

--- aidiscuss-frontend/micSwitchInfo.js
+++
@@ -1,8 +0,0 @@
-// 定义MicSwitchInfo结构
-class MicSwitchInfo {
-    constructor(externMic, wireMic, virtualMic) {
-        this.externMic = externMic;
-        this.wireMic = wireMic;
-        this.virtualMic = virtualMic;
-    }
-}
